c0b725a4c4510110f398fc0b398ec9cbcb986f9f
azkahaf <harishazka@gmail.com>
2026-02-20 14:52:06 +0700

    SpringBoot starter

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/bidmartauthentication/BidmartAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmart.bidmartauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartAuthenticationApplication.class, args);
+    }
+
+}
