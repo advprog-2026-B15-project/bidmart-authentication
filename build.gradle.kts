913a127b0cdcb899f2e1538e641bf1ca24e19cdf
azkahaf <harishazka@gmail.com>
2026-04-17 19:58:50 +0700

    Fix SonarCloud CI: upgrade sonarqube plugin to 5.0.0.4638 for Gradle 9 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     checkstyle
     jacoco
-    id("org.sonarqube") version "6.0.1.5171"
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
 group = "id.ac.ui.cs.advprog.bidmart"
