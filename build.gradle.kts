f1f90f80fb76ba996a5fc16a946a70a3f75507a7
azkahaf <harishazka@gmail.com>
2026-04-17 19:55:11 +0700

    Upgrade SonarQube plugin to 6.0.1.5171 for Gradle 9 compatibility
    Plugin 4.4.1.3373 uses getConvention() API removed in Gradle 9.x, causing
    CI SonarCloud analysis to fail. Version 6.x supports Gradle 9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     checkstyle
     jacoco
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.cs.advprog.bidmart"
